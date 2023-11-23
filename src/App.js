5d2bef6fbe8bc00842e16a19df70eb3f2dc8476e
FakeGods <w.leszkowicz99@gmail.com>
2023-11-23 20:05:14 +0100

    Look

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 //import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/NavBar/Nav';
+import { BrowserRouter } from 'react-router-dom';
+import { Navbar, About, Contact, Tech, Projects, Footer }from './components';
+
 
 
 function App() {
   return (
-    
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <Navbar></Navbar>
-
+        <Navbar />
+        <About />
+        <Contact />
+        <Tech />
+        <Projects />
+        <Footer />
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
