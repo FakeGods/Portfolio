a8a52305563c16c2a17d8062824df9b65661d56b
FakeGods <w.leszkowicz99@gmail.com>
2023-11-25 16:00:31 +0100

    trying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-//import logo from './logo.svg';
-import { BrowserRouter } from 'react-router-dom';
-import { Navbar, About, Contact, Tech, Projects, Footer }from './components';
+
+import { Navbar }from './components';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
+
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <About />
-        <Contact />
-        <Tech />
-        <Projects />
-        <Footer />
       </header>
     </div>
-    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-2 fixed 
+      top-0 z-20 bg-flashWhite sm:opacity-[0.97] xxs:h-[12vh]`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+
+    </nav>
+  );
+};
+
+export default Navbar;
