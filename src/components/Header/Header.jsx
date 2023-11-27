073d234aee14d071c565850d76995d7b06198acd
FakeGods <w.leszkowicz99@gmail.com>
2023-11-27 20:32:43 +0100

    Css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link } from 'react-scroll';
+
 
 const Header =() => {
   return (
     <section id="header">
       <div className="headerContent">
         <span className="hello">Front-End React Developer</span>
-        <p className="headerText">Hi. I'm Wiktor Leszkowicz. A begginer React developer basen in Wroclaw, Poland.</p>
-        <Link className='btn'><img src=""/>Hire Me.</Link>
+        <p className="headerText">Hi. I'm Wiktor Leszkowicz. A begginer React developer basen in Wrocław, Poland.</p>
       </div>
       
     </section>
