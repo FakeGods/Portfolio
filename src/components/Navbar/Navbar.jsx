3802f535e893bd8219091553df0a4979b38c8d48
FakeGods <w.leszkowicz99@gmail.com>
2024-01-18 18:24:25 +0100

    hjahahgaha

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
+import React, { useSate } from "react";
+
+import { CiMenuFries } from "react-icons/ci";
+import { RxCross2 } from "react-icons/rx";
+
 const pageUp = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
-const Navbar = () => {
+function Navbar() {
+  const toggleMobileMenu = () => {
+    const mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.classList.toggle('open-menu');
+  };
+
+
   return (
     <>
       <nav className="navbar">
@@ -30,10 +41,43 @@ const Navbar = () => {
               Contact
             </a>
           </li>
+          <li onClick={toggleMobileMenu}>
+            <CiMenuFries className="mobile-menu" width={30} height={30} />
+          </li>
         </ul>
-
       </nav>
+      {/* mobile nav */}
+      <div className="mobile-nav closed-menu">
+        <span onClick={toggleMobileMenu}>
+          <RxCross2 width={30} height={30} />
+        </span>
+
+           <ul className="desktopMenu-mobile">
+            <li>
+              <a className="desktopMenuListItem" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="desktopMenuListItem" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="desktopMenuListItem" href="#projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="desktopMenuListItem" href="#contact">
+                Contact
+              </a>
+            </li>
+
+        </ul>
+      </div>
     </>
+
   );
 };
 
